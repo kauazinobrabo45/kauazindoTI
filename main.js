0d197362883e385f1b6767e519474cf7345b0396
kauazinobrabo45 <128061954+kauazinobrabo45@users.noreply.github.com>
2024-05-16 11:23:43 -0300

    sdfs

--- main.js
+++ main.js
@@ -15,22 +15,22 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-14T00:00:00");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempo0bjetivo1);
 
-function calculaTempo(tempoObjetivo1){
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo1 - tempoAtual;
-    let segundos = Math.floor(tempoFinal/1000); 
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-
-
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
